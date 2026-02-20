0b9b40609d800f00d9014a5b808938a44d6d08a9
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-02-20 10:31:26 +0100

    Add files via upload

--- mazzoleni.cpp
+++ mazzoleni.cpp
@@ -3,9 +3,7 @@
 
 using namespace std;
 
-	
-int main()
-{
+
 struct prodotto
 {
 	string nome;
@@ -13,7 +11,45 @@ struct prodotto
 	string categoria;
 };
 
+
+void aggiungi( prodotto x, prodotto v[], int d)
+{
+	
+}
+
+
+void visualizza ( prodotto v[], int d)
+{
+	
+}
+
+
+int cerca( prodotto x, prodotto v[], int d)
+{
+	
+}
+
+
+bool cancella( prodotto x, prodotto v[], int d)
+{
+	
+}
+
+bool modifica( prodotto x, prodotto v[], int d)
+{
+	
+}
+	
+	
+	
+	
+	
+int main()
+{
+
+
 prodotto supermercato[100];
+
 
 
     return 0;
