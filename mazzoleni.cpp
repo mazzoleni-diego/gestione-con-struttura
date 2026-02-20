2b0bff271a9e1c061326bc283a6be2e45306f12a
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-02-20 10:23:13 +0100

    Add files via upload

--- mazzoleni.cpp
+++ mazzoleni.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 	
 int main()
 {
+struct prodotto
+{
+	string nome;
+	float prezzo;
+	string categoria;
+};
+
+prodotto supermercato[100];
 
 
     return 0;
