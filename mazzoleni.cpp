056e3c6ec273ed66aadbb2654b59c7ac0b5ad744
mazzoleni <mazzoleni.diego.studente@itispaleocapa.it>
2026-02-20 10:57:13 +0100

    Add files via upload

--- mazzoleni.cpp
+++ mazzoleni.cpp
@@ -32,12 +32,12 @@ int cerca( prodotto x, prodotto v[], int d)
 
 bool cancella( prodotto x, prodotto v[], int d)
 {
-	
+	return false;
 }
 
 bool modifica( prodotto x, prodotto v[], int d)
 {
-	
+	return true;
 }
 	
 	
@@ -46,10 +46,51 @@ bool modifica( prodotto x, prodotto v[], int d)
 	
 int main()
 {
-
-
 prodotto supermercato[100];
-
+int d=sizeof(supermercato)/sizeof(supermercato[0]);
+int opzione;
+prodotto x;
+	do{
+		//visualizzazione voci
+		cout<<"1 - aggiungi";
+		//altre voci
+		cout<<"2 - visualizza";
+		cout<<"3 - cerca";
+		cout<<"4 - cancella";
+		cout<<"5 - modifica";
+		cout<<"0 - finisce il programma";
+		cin>>opzione;
+		switch(opzione)
+		{
+			case 1://operazione 1
+			    
+				aggiungi(x, supermercato, d);
+				break;
+				
+			case 2://operazione 2
+			
+				visualizza(supermercato, d);
+				break;
+					
+			case 3://operazione 3
+			
+				cerca(x, supermercato, d);
+				break;
+				
+			case 4://operazione 4
+			
+				cancella(x, supermercato, d);
+				break;
+					
+			case 5://operazione 5
+			
+				modifica(x, supermercato, d);
+				break;
+				
+				
+		}
+		
+	}while(opzione!=0);
 
 
     return 0;
